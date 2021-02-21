461661e93c67312e769a149ab686835889641cb7
최순원 <>
2021-02-21 16:15:36 +0900

    메모앱 기초 프로젝트 파일(MVVM, UnitTest)

---
+++ MEMO-MVVM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MEMO-MVVM
+//
+//  Created by 최순원 on 21/02/2021.
+//  Copyright © 2021 최순원. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MEMO-MVVMTests/MEMO_MVVMTests.swift
@@ -0,0 +1,34 @@
+//
+//  MEMO_MVVMTests.swift
+//  MEMO-MVVMTests
+//
+//  Created by 최순원 on 21/02/2021.
+//  Copyright © 2021 최순원. All rights reserved.
+//
+
+import XCTest
+@testable import MEMO_MVVM
+
+class MEMO_MVVMTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
